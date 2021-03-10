620caf08cd3d8331da9d1288bcbd95592ffb0032
kny <kny030303>
2021-03-10 22:23:59 +0900

    javascript variable

---
+++ javaScript_Ex/variable.js
@@ -0,0 +1,18 @@
+//why?
+//JavaScript is very flexible
+//flexible == dangerous
+//added ECMAScript 5
+'use strict';
+
+// 2.Variable
+//let (added in ES6)
+{
+    let name = 'nayoon';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+}
+
+console.log(name);
+
+

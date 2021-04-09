4ee57439056e62f0929eb862a17e2e40cc40f813
nayoon030303 <nayoon030303@gmail.com>
2021-04-09 23:50:51 +0900

    input

---
+++ javaScript_Ex/jsInput.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const readline = require('readline');
+
+//인터페이스 객체를 만들자.
+const rl = readline.createInterface({
+    input: process.stdlin,
+    output: process.stdout
+});
+
+
+//정수하나 입력받기 
+let n;
+rl.on(log)

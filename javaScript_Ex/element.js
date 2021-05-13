3eabe797796433f177e8add739fd67dc34ee8763
nayoon030303 <nayoon030303@gmail.com>
2021-05-13 11:45:25 +0900

    code divide && event bindings

--- javaScript_Ex/element.js
+++ javaScript_Ex/element.js
@@ -1,12 +1,20 @@
-function printResult(){
-    btnPrint.value = "냨 클릭되었습니당";
-    span1.innerText = "안녕하세요"
-
-    //btnPrint.type = "text";
-}
 
 function init(){
-    btnPrint.onclick = printResult;
+    let btnPrint = document.getElementById("btn-print");
+
+    let add = function(x,y){
+        return x+y;
+    }
+
+    btnPrint.onclick = function printResult(){
+
+        let btnPrint = document.getElementById("btn-print");
+
+        btnPrint.value = "냨 클릭되었습니당";
+        span1.innerText = "안녕하세요"
+    
+    };
 }
 
-window.onload = init;
+//window.onload = init;
+window.addEventListener("load",init);
---
+++ javaScript_Ex/test.js
@@ -0,0 +1,5 @@
+function init(){
+    alert("안녕하세요!");
+};
+
+window.addEventListener("load",init);

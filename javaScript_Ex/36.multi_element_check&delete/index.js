70de874500fe5ae7dccc3bb14a3d8421661d161d
nayoon030303 <nayoon030303@gmail.com>
2021-05-14 08:49:15 +0900

    두 element 교환

--- javaScript_Ex/36.multi_element_check&delete/index.js
+++ javaScript_Ex/36.multi_element_check&delete/index.js
@@ -18,15 +18,30 @@ const init = function(){
     delBtn.onclick = function(){
         
         let inputs = tbodyNode.querySelectorAll("input[type='checkbox']:checked"); //선택된 checkbox만 가져옴
-        console.log(inputs.length);
         for(let i=0; i<inputs.length; i++){
             inputs[i].parentElement.parentElement.remove();
         }
-    }
+    };
 
     swapBtn.onclick = function(){
+        let inputs = tbodyNode.querySelectorAll("input[type='checkbox']:checked");
+        console.log(inputs.length);
+        if(inputs.length!==2){
+            alert("엘리먼트는 2개를 선택해야만 합니다.");
+            return;
+        }
 
-    }
-}
+        let trs = [];
+        for(let i=0; i<inputs.length; i++){
+            trs.push(inputs[i].parentElement.parentElement);
+        }
+
+        //두개의 element 위치 바꾸기 
+        let cloneNode = trs[0].cloneNode(true);
+        trs[1].replaceWith(cloneNode);
+        trs[0].replaceWith(trs[1]);
+
+    };
+};
 
 window.addEventListener('load',init);

9008f4bbda861cb6c7b89c2335eac5a8ca7fed1d
nayoon030303 <nayoon030303@gmail.com>
2021-05-13 14:44:16 +0900

    chage img_element.value&&list

--- javaScript_Ex/30.changeNode/index.js
+++ javaScript_Ex/30.changeNode/index.js
@@ -2,12 +2,16 @@
 
 function init(){
     let section = document.querySelector("#section5");
+    let imgSelect = document.querySelector(".img-select");
     let srcInput = section.querySelector(".src-input");
     let changeButton = section.querySelector(".change-button");
     let img = section.querySelector(".img");
 
     changeButton.onclick = function(){
-        img.src = `img/${srcInput.value}.jpg`
+        //img.src = `img/${srcInput.value}.jpg`;
+        //img.src = `img/${imgSelect.value}`;
+        img.src = `img/${srcInput.value}`;
+
         console.log(img.src);
     }
 }

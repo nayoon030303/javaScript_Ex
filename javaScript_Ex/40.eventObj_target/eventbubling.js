41e7b5a6a447c2a185ca017b870af61f655cbaa5
nayoon030303 <nayoon030303@gmail.com>
2021-05-14 10:18:52 +0900

    event bubbling

---
+++ javaScript_Ex/40.eventObj_target/eventbubling.js
@@ -0,0 +1,20 @@
+
+
+
+//Ex2-버블링을 이용한 사용자 이벤트 처리하기
+function init(){
+    const section = document.querySelector("#section2");
+    let imgList = section.querySelector(".img-list");
+    let currentImg = section.querySelector(".current-img");
+
+   
+
+    imgList.onclick = function(e){
+        if(e.target.nodeName !="IMG")return;
+        currentImg.src = e.target.src;
+    };
+    
+}
+
+
+window.addEventListener("load",init);

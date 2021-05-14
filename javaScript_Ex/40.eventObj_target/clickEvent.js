5db9f25888dae2ce45b3745bf42faacd0c3eb3c3
nayoon030303 <nayoon030303@gmail.com>
2021-05-14 13:46:53 +0900

    clientX/Y,pageX/Y,offsetX/Y

--- javaScript_Ex/40.eventObj_target/clickEvent.js
+++ javaScript_Ex/40.eventObj_target/clickEvent.js
@@ -5,14 +5,19 @@ function init(){
     const box = section.querySelector(".box");
 
     container.onclick = function(e){
-       // e.x,e.y
-       //e.offsetX, e.offsetY
-       //e.clientX, e.pageX
+       // e.x,e.y : 클라이언트 영억
+       //e.pageX,e.pageY: 스크롤을 내린 페이지의 영역
+       //e.offsetX, e.offsetY: 클릭한 최전방의 기준 영역
+       
 
-       console.log(`${e.x},${e.y}`);
+       console.log(`e: ${e.x},${e.y}`);
+       console.log(`clinet: ${e.clientX},${e.clientY}`);
+       console.log(`page: ${e.pageX},${e.pageY}`);
+       console.log(`offset: ${e.offsetX},${e.offsetY}`);
+
        box.style.position = "absolute";
-       box.style.left = `${e.x}px`; //px단위를 써줘야한다.
-       box.style.top =`${e.y}px`;
+       box.style.left = `${e.pageX}px`; //px단위를 써줘야한다.
+       box.style.top =`${e.pageY}px`;
     };
 };
 
